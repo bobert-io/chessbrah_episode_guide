16c6d72b61ac659b52f8bef9e56d272414ba4477
bob <bob@bobert.io>
2025-03-27 20:41:15 -0400

    .

--- data_collection/script.sh
+++ data_collection/script.sh
@@ -2,6 +2,11 @@ set -x
 set -e
 
 python3 fetch_games.py
+
+# Extremely dirty hack because systemplayer had its name changed on chess.com to systematic
+rm /data/chess_prj/games/systemplayer.json
+cat /data/chess_prj/games/systematic.json | sed 's/systematic/systemplayer/g' > /data/chess_prj/games/systemplayer.json # This is probably all kinds of wrong somehow -- but good nuff
+
 python3 -c "import pandas as pd; open('/data/chess_prj/playlists.txt','wt').write('\n'.join(pd.read_csv('/data/chess_prj/data_sources.csv').playlist.tolist())+'\n')"
 ./yt_dlp_fetch.sh || true
 ./take_screenshots.sh
